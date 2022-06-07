9ff6edd6ff30b1464a7b23e99760c57bf0339787
Arif Babayev <101328505+ArifBabayev05@users.noreply.github.com>
2022-06-07 15:53:32 +0300

    Fix-Animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,13 +82,13 @@ function signIn(e) {
 
 
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("navbar").style.top = "0";
-  } else {
-    document.getElementsByClassName("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementsByClassName("navbar").style.top = "0";
+//   } else {
+//     document.getElementsByClassName("navbar").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
